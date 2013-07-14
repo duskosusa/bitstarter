007d6601d2cc2e168242e8b47bb5cf0e556d7010
Ubuntu <ubuntu@ip-172-31-24-133.us-west-2.compute.internal>
2013-07-14 13:43:02 +0000

    ajmo opet

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html"));
+  	
+	var fs = require ("fs");
+	var sadrzaj = fs.ReadFileSync("index.html", "utf-8");
+
+	response.send(sadrzaj);
 });
 
 var port = process.env.PORT || 5000;
